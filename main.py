a460c52346f0e3f9eb747a865c73232b2cf11dbd
Thomas Holdom <85081553+TepidJesus@users.noreply.github.com>
2022-08-10 15:56:02 +1200

    added testing code
    Just need to create your own .env file and put your API key in it.

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+from urllib import request
+import requests
+import json
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+API_KEY = os.getenv('API_KEY')
+CSGO_APP_ID = 730
+
+
+item_hash = "AK-47 | Redline (Field-Tested)"
+
+request_data_raw = requests.get("https://api.steamapis.com/market/item/{id}/{item}?api_key={key}".format(id=CSGO_APP_ID, item=item_hash, key=API_KEY))
+
+print(request_data_raw.json())
